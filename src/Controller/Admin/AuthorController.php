32ad1efcaaecbd61b416415fc327efc971006afc
dimitrijrt <dimitrijacquart@gmail.com>
2025-03-12 19:58:57 +0100

    add form , request, auteur controller

---
+++ src/Controller/Admin/AuthorController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Author;
+use App\Form\AuthorType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+
+#[Route('/admin/author')]
+final class AuthorController extends AbstractController
+{
+    #[Route('', name: 'app_admin_author', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('admin/author/index.html.twig', [
+            'controller_name' => 'AuthorController',
+        ]);
+    }
+
+
+    #[Route('/new', name: 'app_admin_author_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+        $author = new Author();
+        $form = $this->createForm(AuthorType::class, $author);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+
+           
+        }
+        
+        return $this->render('admin/author/new.html.twig', [
+            'form' => $form,
+
+        ]);
+    }
+
+
+   
+
+}
+
--- src/Form/AuthorType.php
+++ src/Form/AuthorType.php
@@ -8,24 +8,33 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class AuthorType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('nationality')
-            ->add('dateOfBirth', null, [
+            ->add('name', type:TextType::class)
+            ->add('nationality', TextType::class, [
+                 'required' => false,
+            ])              
+            ->add('dateOfBirth', DateType::class, [
+                'input' => 'datetime_immutable',
                 'widget' => 'single_text',
             ])
-            ->add('dateOfDeath', null, [
+            ->add('dateOfDeath', DateType::class, [
                 'widget' => 'single_text',
+                'input' => 'datetime_immutable',
+                'required' => false,
             ])
             ->add('books', EntityType::class, [
                 'class' => Book::class,
                 'choice_label' => 'id',
                 'multiple' => true,
+                'required' => false,
             ])
         ;
     }
